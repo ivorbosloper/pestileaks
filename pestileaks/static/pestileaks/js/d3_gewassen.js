3cd34b4a1e87d5adbd1f1caf5aa67890929f2ef3
Ivor Bosloper <ivorbosloper@gmail.com>
2013-10-31 14:35:09 +0100

    Testje andere view op gewassen

--- pestileaks/static/pestileaks/js/d3_gewassen.js
+++ pestileaks/static/pestileaks/js/d3_gewassen.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 	    color = d3.scale.category20b();
 	
 	var div = d3.select("#vis");
-	
 	var vis = div.append("svg")
 	    .attr("width", width + padding * 2)
 	    .attr("height", height + padding * 2)
@@ -138,4 +137,52 @@ $(document).ready(function(){
 	function brightness(rgb) {
 	  return rgb.r * .299 + rgb.g * .587 + rgb.b * .114;
 	}
+});
+
+/* alternatief op de gewassen, ziet er ook vet uit! */
+$(document).ready(function(){
+	return;
+	var diameter = $("#middelen-graphic").width();
+	
+	var tree = d3.layout.tree()
+	    .size([360, diameter / 2 - 120])
+	    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+	
+	var diagonal = d3.svg.diagonal.radial()
+	    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+	
+	var svg = d3.select("#middelen-graphic").append("svg")
+	    .attr("width", diameter)
+	    .attr("height", diameter)
+	  .append("g")
+	    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+	
+	d3.json("gewassen.json", function(error, root) {
+	  var nodes = tree.nodes({name:'', children:root}),
+	      links = tree.links(nodes);
+	
+	  var link = svg.selectAll(".link")
+	      .data(links)
+	    .enter().append("path")
+	      .attr("class", "link")
+	      .attr("d", diagonal);
+	
+	  var node = svg.selectAll(".node")
+	      .data(nodes)
+	    .enter().append("g")
+	      .attr("class", "node")
+	      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+	
+	  node.append("circle")
+	      .attr("r", 4.5);
+	
+	  node.append("text")
+	      .attr("dy", ".31em")
+	      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+	      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+	      .text(function(d) { return d.name; });
+	});
+	
+	d3.select(self.frameElement).style("height", diameter - 150 + "px");
 });
