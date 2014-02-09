9cf1f32aa2319502166adc90f462291de14a452e
Ivor Bosloper <ivorbosloper@gmail.com>
2014-02-10 00:06:13 +0100

    Inlezen middelen rechtstreeks van CTGB site, moet nog extra attributen meenemen (expiratie, toegelaten, etc)

--- pestileaks/management/commands/leesGewas.py
+++ pestileaks/management/commands/leesGewas.py
@@ -6,8 +6,9 @@ import re
 
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
-    
+
     def handle(self, *args, **options):
         doc = xlrd.open_workbook("doc/dtg/EdiTeelt_DR_Mestwet_gewassen.xls")
         for sheet in doc.sheets():
@@ -16,7 +17,7 @@ class Command(BaseCommand):
                 if not sheet.cell_value(rx, 4): continue
                 print "sheet.name == %s" % sheet.name
                 skip_hij = 3 if sheet.name == '7. Diversen' else 0
-                
+
                 # scan first 3 cells for groups
                 for i in range(3):
                     group = sheet.cell_value(rx, i)
@@ -24,44 +25,44 @@ class Command(BaseCommand):
                         code, naam = re.split('\s+', group, 1)
                         if code.endswith('.'):
                             code = code[:-1]
-                        edi_code = code.replace('.','0')
-                        print dict(edi_code=edi_code, edi_naam=naam, niveau=i+1)
-                        Gewas.objects.create(edi_code=edi_code, edi_naam=naam, niveau=i+1)
+                        edi_code = code.replace('.', '0')
+                        print dict(edi_code=edi_code, edi_naam=naam, niveau=i + 1)
+                        Gewas.objects.create(edi_code=edi_code, edi_naam=naam, niveau=i + 1)
 
-                print dict(niveau=4, 
-                                     edi_code=sheet.cell_value(rx, 4), 
-                                     edi_naam=sheet.cell_value(rx, 3), 
+                print dict(niveau=4,
+                                     edi_code=sheet.cell_value(rx, 4),
+                                     edi_naam=sheet.cell_value(rx, 3),
                                      edi_periode=sheet.cell_value(rx, 5),
                                      edi_teeltdoel=sheet.cell_value(rx, 6),
-                                     
+
 #                                     teelt_onbedekt=sheet.cell_value(rx, 7) != '',
 #                                     teelt_bedekt=sheet.cell_value(rx, 8) != '',
 #                                     teelt_opkweek_bedekt=sheet.cell_value(rx, 9) != '',
 
-                                     dr_gewas_object = sheet.cell_value(rx, 10-skip_hij),
-                                     dr_opmerkingen_teeltdoel = sheet.cell_value(rx, 11-skip_hij),
-                                     dr_code = sheet.cell_value(rx, 12-skip_hij),
+                                     dr_gewas_object=sheet.cell_value(rx, 10 - skip_hij),
+                                     dr_opmerkingen_teeltdoel=sheet.cell_value(rx, 11 - skip_hij),
+                                     dr_code=sheet.cell_value(rx, 12 - skip_hij),
 
-                                     mest_gewas_object = sheet.cell_value(rx, 13-skip_hij),
-                                     mest_opmerkingen_teeltdoel = sheet.cell_value(rx, 14-skip_hij),
-                                     mest_code = sheet.cell_value(rx, 15-skip_hij))
-                
+                                     mest_gewas_object=sheet.cell_value(rx, 13 - skip_hij),
+                                     mest_opmerkingen_teeltdoel=sheet.cell_value(rx, 14 - skip_hij),
+                                     mest_code=sheet.cell_value(rx, 15 - skip_hij))
 
-                Gewas.objects.create(niveau=4, 
-                                     edi_code=str(int(sheet.cell_value(rx, 4))), 
-                                     edi_naam=sheet.cell_value(rx, 3), 
+
+                Gewas.objects.create(niveau=4,
+                                     edi_code=str(int(sheet.cell_value(rx, 4))),
+                                     edi_naam=sheet.cell_value(rx, 3),
                                      edi_periode=sheet.cell_value(rx, 5),
                                      edi_teeltdoel=sheet.cell_value(rx, 6),
-                                     
+
 #                                     teelt_onbedekt=sheet.cell_value(rx, 7) != '',
 #                                     teelt_bedekt=sheet.cell_value(rx, 8) != '',
 #                                     teelt_opkweek_bedekt=sheet.cell_value(rx, 9) != '',
 
-                                     dr_gewas_object = sheet.cell_value(rx, 10-skip_hij),
-                                     dr_opmerkingen_teeltdoel = sheet.cell_value(rx, 11-skip_hij),
-                                     dr_code = int(sheet.cell_value(rx, 12-skip_hij)),
+                                     dr_gewas_object=sheet.cell_value(rx, 10 - skip_hij),
+                                     dr_opmerkingen_teeltdoel=sheet.cell_value(rx, 11 - skip_hij),
+                                     dr_code=int(sheet.cell_value(rx, 12 - skip_hij)),
 
-                                     mest_gewas_object = sheet.cell_value(rx, 13-skip_hij),
-                                     mest_opmerkingen_teeltdoel = sheet.cell_value(rx, 14-skip_hij),
-                                     mest_code = sheet.cell_value(rx, 15-skip_hij))
-                
+                                     mest_gewas_object=sheet.cell_value(rx, 13 - skip_hij),
+                                     mest_opmerkingen_teeltdoel=sheet.cell_value(rx, 14 - skip_hij),
+                                     mest_code=sheet.cell_value(rx, 15 - skip_hij))
+
---
+++ pestileaks/management/commands/leesMiddel.py
@@ -0,0 +1,129 @@
+import logging
+from django.core.management.base import BaseCommand
+from pestileaks.models import Middel
+import requests
+import unicodecsv
+import codecs
+
+logger = logging.getLogger(__name__)
+BASE_URL = "http://www.ctgb.nl/ctb_files/"
+FormuleringNaarEenheid = {
+    'granulaat of korrel': 'kg/ha',
+    'Granulaat': 'kg/ha',
+    'Suspensie concentraat': 'l/ha',
+    'vloeistof': 'l/ha',
+    'Wateroplosbaar poeder': 'kg/ha',
+    'Wateroplosbaar granulaat': 'kg/ha',
+    'Spuitpoeder': 'kg/ha',
+    'Met water mengbaar concentraat': 'l/ha',
+    'Andere vloeistoffen voor directe toepassing': 'l/ha',
+    'lokaas': 'kg/ha',
+    'Tablet': 'kg/ha',
+    'Emulgeerbaar concentraat': 'l/ha',
+    'Water dispergeerbaar granulaat': 'kg/ha',
+    'doekje': 'kg/ha',
+    'Lokmiddel op graanbasis': 'kg/ha',
+    'Lokmiddel (klaar voor gebruik)': 'kg/ha',
+    'Gas (onder druk)': 'l/ha',
+    'pompspray': 'l/ha',
+    'Suspensie concentraat voor zaadbehandeling': 'l/ha',
+    'Pasta op waterbasis': '',
+    'Stuifpoeder': 'kg/ha',
+    'Aerosol spuitbus': '',  # geen idee
+    'Water dispergeerbaar poeder voor vochtige zaadbehandeling': 'kg/ha',
+    'tablet': 'kg/ha',
+    'Emulsie, olie in water': 'l/ha',
+    'Oplossing voor plaatselijke huidbehandeling': 'l/ha',
+    'Strooipoeder': 'kg/ha',
+    'Tablet en Granulaat': 'kg/ha',
+    'Damp ontwikkelend product': '',  # kg of l, stoffen: m of kg
+    'Diversen': 'kg/ha',
+    'Lokmiddel in korrelvorm': 'kg/ha',
+    'Lokmiddel in blokvorm': 'kg/ha',
+    'vloeibaar': 'l/ha',
+    'Plantenstaafje': '',
+    'Capsule suspensie': 'l/ha',
+    'Emulsie voor zaadbehandeling': '',
+    'Emulsie, water in olie': 'l/ha',
+    'poeder': 'kg/ha',
+    'Fijn granulaat': 'kg/ha',
+    'Suspo-emulsie': 'l/ha',
+    'Olie dispersie': '',
+    'Oplossing voor ULV toepassing': 'l/ha',
+    'Dispergeerbaar concentraat': 'l/ha',
+    'Filmvormer': 'l/ha',
+    'Koud vernevelbaar concentraat': 'l/ha',
+    'Wateroplosbaar tablet': 'kg/ha',
+    'Contact vloeistof of gel': '',
+    'Heet vernevelbaar concentraat': 'l/ha',
+    'Micro emulsie': '',
+    'capsule': '',  # geen idee
+    'Combiverpakking vloeibaar/vloeibaar': 'l/ha',
+    'Andere poeders': 'kg/ha',
+    'Ingekapseld granulaat': 'kg/ha',
+    'Poeder voor droge zaadbehandeling': 'kg/ha',
+    'schubben': 'kg/ha',
+    'Rookontwikkelaar': 'kg/ha',
+    'verspuitbaar poeder': 'kg/ha',
+    'Gasontwikkelend product': 'kg/ha',
+    'Technisch concentraat': 'l/ha',
+    'Oplossing voor zaadbehandeling': 'l/ha',
+    'rookmiddel': '',
+    'aerosol': '',
+    'patroon': '',
+    'pasta': '',
+    'staaf': '',
+    'verstuiver': '',
+    'stift': '',
+    'oormerk': '',
+    'geimpregneerde (poreuze) plaat': '',
+    'strip': '',
+    'verdampingsmiddel': '',
+    'halsband': '',
+    'gas': '',
+    'plug': '',
+    'gel': '',
+    'tube': '',
+    'suspensie': 'l/ha',
+    'Concentraat voor lokmiddel': 'l/ha',
+    'Rooktablet': '',  # geen idee
+    'Rookpatroon': '',  # geen idee
+    'Emulgeerbaar granulaat': 'kg/ha',
+    'Microgranulaat': 'kg/ha',
+    'Olie mengbaar concentraat': 'l/ha'}
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        lists = {}
+
+        for f in ["toegelaten_middelen", "vervallen_middelen"]:
+            url = "%s%s.xls" % (BASE_URL, f)
+            r = requests.get(url, stream=True)
+            if r.status_code != 200:
+                raise Exception("Kan bestand niet ophalen bij ctgb: %s" % url)
+            reader = unicodecsv.DictReader(r.raw, encoding='LATIN-1', delimiter='\t', quotechar='"')
+            lists[f] = list(reader)
+
+        for f, lst in lists.items():
+            for row in lst:
+#                if not 'professioneel' in row['toepassing'].lower().replace('niet-professioneel', ''):
+#                    continue
+                formulering = row['formulering']
+                formulering_eenheid = FormuleringNaarEenheid.get(formulering, None)
+                eenheid = formulering_eenheid or 'l/ha'
+
+                middel = Middel.objects.filter(toelatings_nummer=row['toelatingnr'])
+                middel = middel[0] if middel else Middel()
+
+                # # (id, naam, toelatings_nummer, bedrijf, eenheid)
+                middel.naam = unicode(row['middelnaam'][:50]).capitalize()
+                middel.eenheid = eenheid
+                middel.toelatings_nummer = row['toelatingnr']
+                middel.bedrijf = row['toelatinghouder'][:50]
+                # middel.vervallen = (file == 'vervallen_middelen')
+                middel.save()
+
+            logger.info("Updated %d items for list %s" % (len(lst), f))
+
