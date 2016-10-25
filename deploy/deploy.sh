2005e18324d7b5eccdd11a38270c127ee5173f26
damaarten <maarten@crop-r.com>
2016-10-25 23:54:01 +0200

    geen gedoe met tag live_2013 meer nodig, na manage.py migrate werkt de master branch gewoon weer

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,5 @@
 cd /home/pestileaks/pestileaks
-git pull origin live_2013
-git checkout live_2013
-# This branch's doc/requirements.txt is missing some dependencies. Use requirements.txt from the master branch.
-git fetch
-git checkout origin/master -- doc/requirements.txt
+git pull origin master
 . ../env/bin/activate 
 pip install -r doc/requirements.txt
 ./manage.py collectstatic --noinput
