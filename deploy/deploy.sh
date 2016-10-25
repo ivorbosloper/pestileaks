0ec7840cb1ad91f4e79f53d1ed4ca7535d3a96cb
damaarten <maarten@crop-r.com>
2016-10-25 23:20:19 +0200

    fixes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 cd /home/pestileaks/pestileaks
 git pull origin live_2013
+git checkout live_2013
 # This branch's doc/requirements.txt is missing some dependencies. Use requirements.txt from the master branch.
 git fetch
 git checkout origin/master -- doc/requirements.txt
